a528debf73fbe473f068a7a6df94ba6d0b32e6af
Tristan. M <ghoststeam217@gmail.com>
2017-02-18 11:58:19 +1300

    Removed Description
    Going to be moved to a seperate documentation file.

--- class.scissors.php
+++ class.scissors.php
@@ -2,25 +2,15 @@
 	
 	//--------------------------------------------------
 	
-	/* 
-	 * This class was created by Tristan Mastrodicasa as a 
-	 * project to help manage html across a website. It helps
-	 * maintain correct links and should mitigate the problems 
-	 * of having to copy-paste HTML code across multiple webpages 
-	 * just to have a headache when you want to change something 
-	 * later on.
-	 */
-	 
 	 /* 
-	  * This class should be used in conjunction with apache's 
-	  * RewriteEngine to keep all code and html processing in a 
-	  * single file which collects $_GET variables for page 
-	  * navigation
+	  * Created by Tristan Mastrodicasa
 	  */
 	
 	//--------------------------------------------------
 	
 	class Scissors {
+		
+		
 		
 	}
 	
